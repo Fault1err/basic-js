65ab4104df66e97449ee0bc00e96e8fe0b46bf9d
Fault1err <oalkozlova@gmail.com>
2022-10-17 03:25:10 +0300

    feat: update files

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here_
+function countCats(matrix) {
+ matrix.filter(el => el === "^^"); 
+  console.log(matrix)
+  return matrix = arr.length;
 }
 
 module.exports = {
